dbdb9b7028f945f04efa7eea2cf47a0beee3b31b
Allie Jones <allie@alliejon.es>
2016-06-04 16:38:22 -0400

    WIP drawing something (but not the right thing)

--- src/dithering.js
+++ src/dithering.js
@@ -1,29 +1,69 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
+
+class Pixel {
+  constructor(values) {
+    this.values = values;
+  }
+
+  brightness() {
+    // TODO: use a better conversion
+    return Math.max(this.values[0], this.values[1], this.values[2]);
+  }
+}
+
+const ditherMatrix = [
+  [6, 8, 4],
+  [1, 0, 3],
+  [5, 2, 7],
+];
+
+const ditherPatterns = (function (matrix) {
+  const intensityLevels = matrix.length * matrix.length;
+  const patterns = [];
+  for (let i = 0; i < intensityLevels; i++) {
+    patterns.push(ditherMatrix.map(row => {
+      return row.map(v => {
+        if (v > i) return new Pixel([255, 255, 255, 255]);
+        return new Pixel([0, 0, 0, 255]);
+      });
+    }));
+  }
+  return patterns;
+}(ditherMatrix));
+
 class DImage {
-  canvas = null;
-  ctx = null;
   width = null
   height = null;
 
-  ready = null;
+  VALS_PER_PIXEL = 4;
 
-  constructor(imageSrc) {
-    this.ready = new Promise((resolve, reject) => {
-      this.canvas = document.createElement('canvas');
-      this.ctx = this.canvas.getContext('2d');
+  constructor(data) {
+    if (data) {
+      this.data = data;
+      this.width = data.length;
+      this.length = data[0].length;
+    } else {
+      this.data = [];
+    }
+  }
+
+  loadFromURL(imageSrc) {
+    return new Promise((resolve, reject) => {
+      const canvas = document.createElement('canvas');
+      const ctx = canvas.getContext('2d');
 
       const image = new Image();
       image.onload = () => {
-        this.width = this.canvas.width = image.width;
-        this.height = this.canvas.height = image.height;
+        this.width = canvas.width = image.width;
+        this.height = canvas.height = image.height;
 
-        this.ctx.drawImage(image, 0, 0);
+        ctx.drawImage(image, 0, 0);
 
-        this.data = this.ctx.getImageData(0, 0, this.width, this.height).data;
+        this.setDataFrom1DArray(ctx.getImageData(0, 0, this.width, this.height).data, this.width, this.height);
 
-        resolve();
+        resolve(this);
       };
 
       image.onerror = reject;
@@ -32,28 +72,27 @@ class DImage {
     });
   }
 
-  VALS_PER_PIXEL = 4;
-
-  getIndex(x, y) {
-    const rowOffset = y * this.VALS_PER_PIXEL * this.width;
-    return rowOffset + (x * this.VALS_PER_PIXEL);
+  setDataFrom1DArray(sourceData, width, height) {
+    const data = [];
+    for (let x = 0; x < width * this.VALS_PER_PIXEL; x += this.VALS_PER_PIXEL) {
+      const offset = x * width;
+      const col = [];
+      for (let y = 0; y < height * this.VALS_PER_PIXEL; y += this.VALS_PER_PIXEL) {
+        col.push(new Pixel(sourceData.slice(offset + y, offset + y + this.VALS_PER_PIXEL)));
+      }
+      data.push(col);
+    }
+    this.data = data;
   }
 
   getPixel(x, y) {
-    if (x >= this.width || y >= this.height) return null;
-
-    const start = this.getIndex(x, y);
-    return this.data.slice(start, start + this.VALS_PER_PIXEL);
+    return this.data[x][y];
   }
 
   setPixel(x, y, value) {
-    if (x >= this.width || y >= this.height) return;
-
-    const index = this.getIndex(x, y);
-    this.data[index] = value;
+    this.data[x][y] = value;
   }
 
-  /* TODO: have this return a new instance of DImage instead */
   getRegion(x, y, size) {
     const region = [];
     for (let dx = 0; dx < size; dx++) {
@@ -63,16 +102,41 @@ class DImage {
       }
       region.push(col);
     }
-    return region;
+    return new DImage(region);
   }
-}
 
-// should be in DImage but ImageData being write-only makes things harder ...
-function setRegion(data, x, y, values) {
-  for (let dx = 0; dx < values.length; dx++) {
-    for (let dy = 0; dy < values[dx].length; dy++) {
-      data[x + dx][y + dy] = values[dx][dy];
+  setRegion(x, y, values) {
+    for (let dx = 0; dx < values.length; dx++) {
+      for (let dy = 0; dy < values[dx].length; dy++) {
+        // TODO: should initialize data somewhere instead of doing this
+        if (!this.data[x + dx]) this.data[x + dx] = [];
+        this.data[x + dx][y + dy] = values[dx][dy];
+      }
     }
+  }
+
+  averageBrightness() {
+    let sum = 0;
+    for (let x = 0; x < this.data.length; x++) {
+      for (let y = 0; y < this.data[x].length; y++) {
+        sum += this.data[x][y].brightness();
+      }
+    }
+    return sum / (this.data.length * this.data.length);
+  }
+
+  toUint8ClampedArray() {
+    const arr = [];
+    for (let x = 0; x < this.data.length; x++) {
+      for (let y = 0; y < this.data[x].length; y++) {
+        const vals = this.data[x][y].values;
+        arr.push(vals[0]);
+        arr.push(vals[1]);
+        arr.push(vals[2]);
+        arr.push(vals[3]);
+      }
+    }
+    return new Uint8ClampedArray(arr);
   }
 }
 
@@ -81,51 +145,38 @@ function lerp(value, sourceStart, sourceEnd, targetStart, targetEnd) {
     (sourceEnd - sourceStart) + targetStart;
 }
 
-function getDitherPattern(intensity, matrix) {
-  return matrix.map();
-}
-
 function dither(image, matrix) {
   const size = matrix.length;
   const intensityLevels = (size * size) - 1;
-  const ditheredImageData = new Uint8ClampedArray();
+  const ditheredImage = new DImage();
+  ditheredImage.width = image.width;
+  ditheredImage.height = image.height;
 
   for (let x = 0; x < Math.floor(image.width / size); x++) {
     for (let y = 0; y < Math.floor(image.height / size); y++) {
-      const intensity = Math.round(lerp(average(image.getRegion(x, y, size)),
-        0, 255, 0, intensityLevels));
-      setRegion(ditheredImageData, x, y, matrix[intensity]);
+      const region = image.getRegion(x, y, size).averageBrightness();
+      const intensity = Math.round(lerp(region, 0, 255, 0, intensityLevels));
+      ditheredImage.setRegion(x, y, ditherPatterns[intensity]);
     }
   }
 
   canvas.width = image.width;
   canvas.height = image.height;
-  const ditheredImage = ctx.createImageData
+  ctx.putImageData(new ImageData(ditheredImage.toUint8ClampedArray(), 168, 224), 0, 0);
 }
 
 function average(imageData) {
   let sum = 0;
   for (let x = 0; x < imageData.length; x++) {
     for (let y = 0; y < imageData[x].length; y++) {
-      sum += brightness(imageData[x][y]);
+      sum += imageData[x][y].brightness();
     }
   }
   return sum / (imageData.length * imageData.length);
 }
 
-function brightness(pixel) {
-  // TODO: use a better conversion
-  return Math.max(pixel[0], pixel[1], pixel[2]);
-}
-
-const ditherMatrix = [
-  [6, 8, 4],
-  [1, 0, 3],
-  [5, 2, 7],
-];
-
-const image = new DImage('abe-grayscale.jpg');
-image.ready.then(
+const image = new DImage();
+image.loadFromURL('abe-grayscale.jpg').then(
   () => dither(image, ditherMatrix),
   (e) => console.error(`Failed to load ${e.target.src}`)
 );
